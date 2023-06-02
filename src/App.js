8c1fabee4572c89372c71f9a34f45a8c5fdcb8ae
OksanaKhritonenko <o.khritonenko@gmail.com>
2023-06-02 22:46:47 +0200

    added files Header.js and Haeder.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Search from "./Search";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="Weather-app">
       <Search />
+      <Header />
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+export default function City() {
+  return (
+    <div className="City">
+      <div className="heading" id="city-name">
+        Oslo
+      </div>
+      <div className="date" id="current-date">
+        Thursday 15:15
+      </div>
+    </div>
+  );
+}
